ad17157505b662ee86d730303d5934e3af598e7a
James Lee <jalee@jalee-mac.tsi.lan>
2017-03-17 16:36:50 -0700

    Revert "Chage buildRequestString to buildSearchRequest"
    This reverts commit 9e596a6138d5bcbd348faf44ff8de3a33263fc69.

--- src/shim/ShimDebug.ts
+++ src/shim/ShimDebug.ts
@@ -30,14 +30,14 @@ export class ShimDebug implements ShimInterface {
     log('GetAuthInfo shim method invoked. Returning dummy data.');
     const auth: AuthInfo = {
       userId: 'dummy@gmail.com',
-      accessToken: 'EwBAA8l6BAAU7p9QDpi/D7xJLwsTgCg3TskyTaQAAaHaTB3A xwQOthpE0Hegze2CeYrvwkZbIc/DAraoGdkp6yqQOOWNc4FB9cKr3kP7s6jVgeNtPEjSkAMJYzM7zQ/j/tEtwcpwtk pF6y3c6SawApQenEmroIae yF4bUg16ugJIIrFrjhZGu25suRuO4coSFOehIxZOekAhylTrnaLxhQSBzK//jj4AeuLaVugxyvCWz73K wNb5ssL9Q806LuQ/OsQbtcliuz82XfiJUIsVEW/K L/Jv78/RmKnte4SErs8NNaoXuQ1U7bG9y3YJgAFQ4Nbqi3bowy6wjexmskp3FB /xcGu8b1RJlr3RLwRI5uzeTrTlSYWCYZ YMDZgAACKOdRExa4awmEAI5VuICZkiQ935mkqucCUCtCIBPeybgZrp63EFw5yps9XBxNGBKyn9I8110IrXACgJfxVCkgqkPgDHaAWYmNes6is2fxpSa4sw/XfIOWOv5fC35SmLl0y98df28WilGYqByhWC6gklDZVwcbP6a0jYo2nQAOaWoEWrHOa/L7plO5LPS iGoD4U 2GUZbcARK6n7etQH0tVJoOXXhkxXvpVUM QveJfITpZTzj j7FTpO8gCaHtEOrUqLQwMnLmepUCPhALqlWZKsNkT0ChDexlV4lofi7YSJcjEybjHtjB8fkFt9Bw2fW3kUz5lSakZ5HleV9BJSx40aRR7GtJfDT UHFOK2XhCf17AtW4o 4aoSVFKJh6oLxrYrQ3jt4QjQRO9EzEGEINXEVtetfo014vqHbbCPT5A8rvv4zcZ3doHAnxZnkUpWsjiCfAom0ObDldhdOl4i6Q/FZtEX3H69AHkl16v0/7sZk1juoG0txr4ulYvFq5LqH1Q6So6dxb2tsIXVyVEMEDtnMzEr2liEZCEaC1jraab1vhEMqRwv1G06FXmLvRSBaUM1G48ApquD8OtPHKEp9gAlgkW1RqugVXrdGSnF1PfmxpouoVY1s3ZEtjfz1cEPIw1kewofdU1zb7vZHPdBSSQOcXWF45qgd2PsuJPMj7WDYWjLm ekRGUY3BXvGGvoAdNSzQt73J0nPFDAg=='
+      accessToken: '0UQLbIImR4gljBygJcgZT2XBHebz8hqY'
     };
     return auth;
   }
 
   public getCloudProvider(): Provider {
     log('GetCloudProvider shim method invoked. Returning dummy data.');
-    return Provider.oneDrive;
+    return Provider.box;
   }
 
   public getConnectedFile(): FileAttrs {
