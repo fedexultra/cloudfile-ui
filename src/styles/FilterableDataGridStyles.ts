d891b993d3cd7687b81a0183eaea73c25fcab822
Aaron Braunstein <abraunstein@tableau.com>
2017-04-17 11:18:30 -0700

    Reorder exports

--- src/styles/FilterableDataGridStyles.ts
+++ src/styles/FilterableDataGridStyles.ts
@@ -40,4 +40,4 @@ const GridStyle: CSSProperties = {
   boxSizing: 'border-box'
 };
 
-export { FilterableDataGridStyle, WrapperStyle, GridStyle };
+export { FilterableDataGridStyle, GridStyle, WrapperStyle };
