24f243afd4359d2b9b3c17f62846a01355cb0a9f
James Lee <jalee@tableau.com>
2017-04-19 17:47:56 -0700

    Create Logger class for logging
    Changed the original log() function to consoleLog(), and made it a
    static method. ShimDebug uses the console log so we want to support that
    functionality here.

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -9,7 +9,49 @@
 //
 // -----------------------------------------------------------------------------
 
-export function log(message: string): void {
-  /* tslint:disable-next-line:no-console */
-  console.log(message);
+import { Severity } from '../types/ShimTypes';
+import { shim } from '../shim/Shim';
+
+
+class Logger {
+
+  public static trace(message: string): void {
+    shim.log({message: '${message}'}, Severity.Trace);
+  }
+
+  public static debug(message: string): void {
+    shim.log({message: '${message}'}, Severity.Debug);
+  }
+
+  public static info(message: string): void {
+    shim.log({message: '${message}'}, Severity.Info);
+  }
+
+  public static warn(message: string): void {
+    shim.log({message: '${message}'}, Severity.Warn);
+  }
+
+  public static error(message: string): void {
+    shim.log({message: '${message}'}, Severity.Error);
+  }
+
+  public static fatal(message: string): void {
+    shim.log({message: '${message}'}, Severity.Fatal);
+  }
+
+  public static off(message: string): void {
+    shim.log({message: '${message}'}, Severity.Off);
+  }
+
+  public static LogDetail(message: string): void {
+    shim.log({message: '${message}'}, Severity.LogDetail);
+  }
+
+  public static consoleLog(message: string): void {
+    // We need this for shim debug
+    /* tslint:disable-next-line:no-console */
+    console.log(message);
+  }
 }
+
+export { Logger };
